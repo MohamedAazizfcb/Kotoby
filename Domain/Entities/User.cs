e6ce1f0fd84eb945bb9f15794a48dedb14b9c794
unknown <mohamedaazizfcb@gmail.com>
2024-07-20 07:13:29 -0700

    UserModel

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Net;
+using System.Reflection;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using System.Xml.Linq;
+using Domain.Enums;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateOfBirt { get; set; }
+        public string ProfilePicture { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastLogin { get; set; }
+        public string AccountStatus { get; set; }
+        public UserRole UserRole { get; set; }
+        public string Address { get; set; }
+        public Gender gender { get; set; }
+        public string Phone { get; set; }
+    }
+}
