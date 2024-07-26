b676daf20fa3685ef6b1d7c4f8772ecd79e54ccd
unknown <mohamedaazizfcb@gmail.com>
2024-07-26 05:51:30 -0700

    PWA

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.MDEV.kotobi',
+  appName: 'kotobi',
+  webDir: 'dist/kotobi/browser',
+  bundledWebRuntime: false
+};
+
+export default config;
